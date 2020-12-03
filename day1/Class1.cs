b1939589a2aec23ff04294b0935d00b4b1cad741
Supersmasher149 <walter.gibbons@gmail.com>
2020-12-02 19:09:32 -0500

    solved problem 1

--- day1/Class1.cs
+++ day1/Class1.cs
@@ -12,38 +12,28 @@ namespace day1
     {
         static void Main(string[] args)
         {
-            //prints as a string
-            //string text = System.IO.File.ReadAllText(@"C:\Users\walte\source\repos\day1\day1\input.txt");
-
+            int Product;
             //reads as an array
             string[] lines = System.IO.File.ReadAllLines(@"C:\Users\walte\source\repos\day1\day1\input.txt");
 
-            /*
+            
             for (int i = 0; i < lines.Length; i++)
             {
                 for (int j = 1; j < lines.Length; j++)
                 {
                     int sum = Int32.Parse(lines[i]) + Int32.Parse(lines[j]);
-                    Console.WriteLine(lines[i], "+", lines[j], "=", sum);
+                    //Console.WriteLine(lines[i], "+", lines[j], "=", sum);
                     if (sum == 2020)
                     {
-                        string x = lines[i];
-                        string y = lines[j];
+                        int x = Int32.Parse(lines[i]);
+                        int y = Int32.Parse(lines[j]);
+                        Console.WriteLine($"yay {lines[i]} + {lines[j]} = {x * y}");
+                        Product = x * y;
                         break;
                     }
                 }
                 Console.WriteLine(lines[i]);
             }
-            */
-
-            //Convert array string into int then add and print
-            int sum = Int32.Parse(lines[0]) + Int32.Parse(lines[1]);
-            Console.WriteLine(sum); 
-
-            //Convert array into string then print
-            //int test = Int32.Parse(lines[1]);
-            //Console.WriteLine(test);
-
             Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
         }
